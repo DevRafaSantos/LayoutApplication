cc0c2bba948cca7579535291b355b57f1a6165da
Bowie <marketing@letscode.com.br>
2024-07-03 13:08:25 -0300

    Criado todo o aplicativo.

---
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.layoutapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.layoutapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClean.setOnClickListener {
+            clean()
+        }
+
+        binding.btnDone.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val nPeopleTemp = binding.tieNumPeople.text
+            val percentageTemp = binding.tiePercentage.text
+
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true ||
+                percentageTemp?.isEmpty() == true
+                ) {
+
+                Snackbar.make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
+                    .show()
+
+            } else {
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Int = nPeopleTemp.toString().toInt()
+                val percentage: Int = percentageTemp.toString().toInt()
+
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+
+                val intent = Intent(this, SummaryActivity::class.java)
+                intent.apply {
+                    putExtra("totalTable", totalTable)
+                    putExtra("numPeople", nPeople)
+                    putExtra("percentage", percentage)
+                    putExtra("totalAmount", totalWithTips)
+                }
+                clean()
+                startActivity(intent)
+
+            }
+
+        }
+
+    }
+    private fun clean () {
+        binding.tieTotal.setText("")
+        binding.tieNumPeople.setText("")
+        binding.tiePercentage.setText("")
+
+    }
+
+}
